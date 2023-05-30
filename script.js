d5cfe3314c0a1aaed1de091acaa802c238e4dd04
MariamAlmagboul <134358386+MariamAlmagboul@users.noreply.github.com>
2023-05-30 11:45:16 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function displayRating() {
+    var rating = document.getElementById("rating").value;
+    document.getElementById("ratingDisplay").innerHTML = "You rated the video " + rating + " star/s :)";
+}
